557a2ec2fd484b398ab9a79f93051337c6f6ac99
neoki <ot07neoki@gmail.com>
2024-10-06 16:53:22 +0900

    feat: add accessibility addon to Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
     "@chromatic-com/storybook",
   ],
   framework: {
